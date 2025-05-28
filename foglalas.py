76ae90288d0ba907dcc5bb4c2314679623a13326
Graffiti302 <martinbebesi09@gmail.com>
2025-05-28 13:12:23 +0200

    +1 extra

--- foglalas.py
+++ foglalas.py
@@ -11,18 +11,23 @@ def Valami():
         print("Ez sajnos nem foglalható. Oszlop van a helyén!")
         Valami()
     if Valasztas == 1 or 2 or 3 or 4 or 6 or 7 or 8:
-        print(f"Köszonjük a vásárlásrt! Az ön széke a/az: {Valasztas}.")
+        Veglegesites =  str(input(f"Biztos hogy ezt a helyet akarod Y/N:({Valasztas})"))
+        if Veglegesites == "N":
+            print("Akkor válassz másikat!")
+            Valami()
+        if Veglegesites == "Y":
+            print(f"Köszonjük a vásárlásrt! Az ön széke a/az: {Valasztas}.")
         
-        szabad = [1,2,3,4,5,6,7,8]
-        Valasztas-1
-        szabad.remove(Valasztas)
-        szabad.append("📗")
-        for i in range (1):
-            print(szabad)
+            szabad = [1,2,3,4,5,6,7,8]
+            Valasztas-1
+            szabad.remove(Valasztas)
+            szabad.append("📗")
+            for i in range (1):
+                print(szabad)
 
     if 1 < Valasztas > 8:
         print("Ez nem opció")
         Valami()
 Valami()
-#szünet
+
 

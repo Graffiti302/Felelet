0c81143ef0c431fd806da336662d4acbfe2b0033
Graffiti302 <martinbebesi09@gmail.com>
2025-05-28 12:21:05 +0200

    #szünet

--- foglalas.py
+++ foglalas.py
@@ -1,8 +1,12 @@
 print("Üdvözlünk a mozi szék foglaló programban!")
 print("Választhatsz egy sor székből a moziteremben!")
-x
+székek = [1, 2, 3, 4, 5, 6, 7, 8]
+Valasztas = int(input("Melyik széket választod?[1-8]:"))
 
-
-
-
+if Valasztas != 1 or 2 or 3 or 4 or 6 or 7 or 8:
+    
+    print("Ez nem opció")
+elif Valasztas == 5:
+    print("Ez sajnos nem foglalható. Oszlop van a helyén!")
+    print(f"Köszonjük a vásárlásrt! Az ön széke a/az: {Valasztas}.")
 

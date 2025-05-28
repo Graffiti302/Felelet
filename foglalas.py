95e1ea51c9c6bc325140f27908b434e4332a0b35
Graffiti302 <martinbebesi09@gmail.com>
2025-05-28 12:09:34 +0200

    #extra

--- foglalas.py
+++ foglalas.py
@@ -2,10 +2,11 @@ print("Üdvözlünk a mozi szék foglaló programban!")
 print("Választhatsz egy sor székből a moziteremben!")
 székek = [1, 2, 3, 4, 5, 6, 7, 8]
 Valasztas = int(input("Melyik széket választod?[1-8]:"))
-if Valasztas != székek:
-    print("Ez nem opció!")
+
+if 1 < Valasztas > 8:
+    print("Ez nem opció")
 else:
-    print("Köszönjük hogy a {székek} szléte választotta!")
-    for i in range (8):
-        print("✔")
-        
+    print(f"Köszonjük a vásárlásrt! Az ön széke a/az: {Valasztas}.")
+
+for i in range (8):
+    print()
